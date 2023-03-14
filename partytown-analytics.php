a3efc62e0479f351c00248977873b72b8b2ab3af
Mohamed Fayis <fayis@outlook.in>
2023-03-14 20:16:15 +0530

    basic configration

---
+++ partytown-analytics.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Partytown Analytics
+ * Plugin URI: https://fayis.in
+ * Description: Partytown Analytics for wordpress
+ * Version: 1.0.0
+ * Author: Mohamed Fayis
+ * Author URI: https://fayis.in
+ * License: Apache License, Version 2.0
+ * License URI: https://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
+
+
+?>
